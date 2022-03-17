5707bae690e85a8158642c5b3d9cd8df9a4ea3ae
Yeeyu <dreamloster@hotmail.com>
2022-03-17 18:29:29 +0800

    1.Nikoyo-common 工程初始化

---
+++ nikoyo-common/src/main/java/com/nikoyo/common/exception/file/FileException.java
@@ -0,0 +1,19 @@
+package com.nikoyo.common.exception.file;
+
+import com.nikoyo.common.exception.base.BaseException;
+
+/**
+ * 文件信息异常类
+ * 
+ * @author ruoyi
+ */
+public class FileException extends BaseException
+{
+    private static final long serialVersionUID = 1L;
+
+    public FileException(String code, Object[] args)
+    {
+        super("file", code, args, null);
+    }
+
+}
