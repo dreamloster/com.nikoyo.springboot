8f1478b20384b699cafc92de73c0694ed85de178
Yeeyu <dreamloster@hotmail.com>
2022-03-22 08:46:32 +0800

    1.工程文件调整 2.配置文件调整

--- nikoyo-api/src/main/java/com/nikoyo/NikoyoApplication.java
+++ nikoyo-api/src/main/java/com/nikoyo/NikoyoApplication.java
@@ -1,8 +1,9 @@
-package com.nikoyo.api;
+package com.nikoyo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 启动程序
